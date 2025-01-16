7a5e0f158baa3ce6558b8e804b9f9567e0cb2081
Lwaiz <1848350910@qq.com>
2025-01-16 12:24:58 +0800

    Ignore cmakecmake-build-debug-wsl directory

--- tests/test_config.cpp
+++ tests/test_config.cpp
@@ -45,17 +45,28 @@ void print_yaml(const YAML::Node& node, int level){
 
 void test_yaml(){
     YAML::Node root = YAML::LoadFile("../bin/conf/log.yml");
-    SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << root;
+    SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << root.Scalar();
 
     print_yaml(root, 0);
 }
 
+void test_config(){
+    SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << "before: " << g_int_value_config->getValue();
+    SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << "before: " << g_float_value_config->toString();
+
+    YAML::Node root = YAML::LoadFile("../bin/conf/log.yml");
+    sylar::Config::LoadFromYaml(root);
+
+    SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << "after: " << g_int_value_config->getValue();
+    SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << "after: " << g_float_value_config->toString();
+}
+
+
 int main(int argc, char** argv) {
 
-    SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << g_int_value_config->getValue();
-    SYLAR_LOG_INFO(SYLAR_LOG_ROOT()) << g_float_value_config->toString();
+    //test_yaml();
 
-    test_yaml();
+    test_config();
 
     return 0;
 }
